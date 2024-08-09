5ec2d87482a273bd358546323bf88b90b62817e4
NoahWhiffen <Noah.Whiffen@keyin.com>
2024-08-08 23:43:52 -0230

    defaults file added and stand fee loop complete

--- Python Final Sprint/StandFee.py
+++ Python Final Sprint/StandFee.py
@@ -21,29 +21,79 @@ def paymentDate():
 
 
 def employeeFile():
-   # global header, driverNum, name, address, phoneNum, licenseNum, licenseExpDate, insProvider, policyNum, carOwner, balDue
+    global empHeader, driverNum, name, address, phoneNum, licenseNum, licenseExpDate, insProvider, policyNum, carOwner, balDue
     try:
         with open("employees.dat", "r") as file:
-            header = "Driver #, Name, Address, Phone #, License #, License Exp. Date, Insurance Provider, Ins. Policy #, Car Owner, Bal. Due"
-            lines = file.readlines.split(", ")
-            driverNum = lines[0]
-            name = lines[1]
-            address = lines[2]
-            phoneNum = lines[3]
-            licenseNum = lines[4]
-            licenseExpDate = lines[5]
-            insProvider = lines[6]
-            policyNum = lines[7]
-            carOwner = lines[8]
-            balDue = lines[9]
-            return header, driverNum, name, address, phoneNum, licenseNum, licenseExpDate, insProvider, policyNum, carOwner, balDue
+            lines = file.readlines()
+            empHeader = lines[0].strip()
+            for line in lines[1:]:
+                employeeData = line.strip().split(", ")
+                driverNum = employeeData[0]
+                name = employeeData[1]
+                address = employeeData[2]
+                phoneNum = employeeData[3]
+                licenseNum = employeeData[4]
+                licenseExpDate = employeeData[5]
+                insProvider = employeeData[6]
+                policyNum = employeeData[7]
+                carOwner = employeeData[8]
+                balDue = employeeData[9]
+                return empHeader, driverNum, name, address, phoneNum, licenseNum, licenseExpDate, insProvider, policyNum, carOwner, balDue
     except FileNotFoundError:
         with open("employees.dat", "a") as file:
-            header = "Driver #, Name, Address, Phone #, License #, License Exp. Date, Insurance Provider, Ins. Policy #, Car Owner, Bal. Due"
-            return header, driverNum, name, address, phoneNum, licenseNum, licenseExpDate, insProvider, policyNum, carOwner, balDue
+            file.write("Driver #, Name, Address, Phone #, License #, License Exp. Date, Insurance Provider, Ins. Policy #, Car Owner, Bal. Due\n")
+
+# def revenueFile():
+#    global revHeader, transID, revDriverNum, transDate, transDesc, transAmt, hst, total
+#    try:
+#        with open("revenue.dat", "r") as file:
+#            lines = file.readlines()
+#            revHeader = lines[0].strip()
+#            for line in lines[1:]:
+#                revenueData = line.strip().split(", ")
+#               transID = revenueData[0]
+#                revDriverNum = revenueData[1]
+#                transDate = revenueData[2]
+#                transDesc = revenueData[3]
+#                transAmt = revenueData[4]
+#                hst = revenueData[5]
+#                total = revenueData[6]
+#                return revHeader, transID, revDriverNum, transDate, transDesc, transAmt, hst, total
+#    except FileNotFoundError:
+#        with open("revenue.dat", "a") as file:
+#                file.write("Trans. ID, Driver #, Trans. Date, Trans. Desc., Trans. Amt., HST, total\n")
+
+# Constants.
+with open("defaults.dat", "r") as file:
+    f = file.readlines()
+    nextTransNum = f[0].strip()
+    nextDriverNum = f[1].strip()
+    standFee = float(f[2].strip())
+    dailyRentFee = float(f[3].strip())
+    weekRentFee = float(f[4].strip())
+    HSTRate = float(f[5].strip())
+
+today = datetime.datetime.today()
+
+# Calculations
+
+standFeeHST = standFee * HSTRate
+standFeeTotal = standFeeHST + standFee
+
 
 # Main program starts here.
 
 while True:
-    
-    if carOwner == name:
+    employeeFile()
+    # revenueFile()
+    if today.day == 1:
+        if name == carOwner:
+            newTransNum = nextTransNum
+            nextTransNum += 1
+            newDriverNum = nextDriverNum
+            nextDriverNum += 1
+            with open("revenue.dat", "a") as f:
+                f.write(f"{nextTransNum}, {paymentDate}, Monthly Stand Fees, {nextDriverNum}, {standFee}, {standFeeHST}, {standFeeTotal} ")
+                
+
+    # The balance due will be updated next.
